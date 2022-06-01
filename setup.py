67956441880310742059e3c272feb59bc138ffd5
michael <mstclair@millionconcepts.com>
2022-06-01 15:23:27 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="marslab",
-    version="0.9.6",
+    version="0.9.8",
     url="https://github.com/millionconcepts/marslab.git",
     author="Million Concepts",
     author_email="chase@millionconcepts.com",
