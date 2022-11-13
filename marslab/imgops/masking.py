08b05a9bd6dfce069f4fa156dc3533bf076288f2
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2022-11-13 14:05:08 -0500

    rework skymask

--- marslab/imgops/masking.py
+++ marslab/imgops/masking.py
@@ -1,5 +1,7 @@
 from _operator import truediv, floordiv
 from functools import reduce
+from inspect import signature
+from types import MappingProxyType
 from typing import Collection
 
 import numpy as np
@@ -10,6 +12,7 @@ from marslab.imgops.imgutils import (
     ravel_valid,
     colorfill_maskedarray,
     cut_annulus,
+    eightbit,
 )
 
 
@@ -82,106 +85,162 @@ def get_widest_section(row):
     widest_section = [
         k for k, v in length_indices.items() if len(v) == widest
     ][0]
-    return tuple(
-        windowed([0, *split_indices, len(row) - 1], 2)
-    )[widest_section]
+    return tuple(windowed([0, *split_indices, len(row) - 1], 2))[
+        widest_section
+    ]
 
 
-def test_vertical_coverage(mask, row_ix, col_indices, cutoff):
-    above = mask[:row_ix, col_indices[0] : col_indices[1]]
-    if (result := (np.nonzero(above)[0].size / above.size)) < cutoff:
-        return False, result
-    return True, result
+def test_axes(mask, shape):
+    vertical, horizontal = shape
+    if (vertical is None) and (horizontal is None):
+        return True
+    col_indices, row_ix = widest_part(mask)
+    if vertical is not None:
+        above = mask[:row_ix, col_indices[0] : col_indices[1]]
+        try:
+            if (np.nonzero(above)[0].size / above.size) < vertical:
+                return False
+        except ZeroDivisionError:
+            pass
+    if horizontal is not None:
+        if ((col_indices[1] - col_indices[0]) / mask.shape[1]) < horizontal:
+            return False
+    return True
 
 
-def test_coverage(mask, cutoff, extent=None, filled=None):
-    if filled is None:
-        filled = floodfill_mask(mask)
-    if extent is None:
-        extent = mask[np.nonzero(mask)].size
-    fill_extent = filled[np.nonzero(filled)].size
-    if (result := (extent / fill_extent)) < cutoff:
-        return False, extent, filled, result
-    return True, extent, filled, result
+def test_coverage(mask, cutoff):
+    if cutoff is None:
+        return True
+    mask_extent = mask[np.nonzero(mask)].size
+    filled_extent = mask[np.nonzero(floodfill_mask(mask))].size
+    if (mask_extent / filled_extent) < cutoff:
+        return False
+    return True
 
 
-def test_extent(mask, cutoff, extent=None):
-    if extent is None:
-        extent = mask[np.nonzero(mask)].size
-    if (result := (extent / mask.size)) < cutoff:
-        return False, extent, result
-    return True, extent, result
+def test_extent(mask, cutoff):
+    if cutoff is None:
+        return True
+    if (mask[np.nonzero(mask)].size / mask.size) < cutoff:
+        return False
+    return True
 
 
 def _blank(shape):
     return np.full(shape, False)
 
 
+def outline(array, edge_params=(60, 100), sigma=5, threshold=25, erosion=3):
+    import cv2
+
+    if array.dtype != np.uint8:
+        array = eightbit(array)
+    edges = cv2.Canny(array, *edge_params)
+    if sigma is not None:
+        edges = ndi.gaussian_filter(edges, sigma=sigma)
+    threshold = centile_threshold(edges, threshold)
+    return maybe_filter(threshold, erosion, filt=ndi.binary_erosion)
+
+
+def maybe_filter(array, size=None, footprint=None, filt=ndi.median_filter):
+    if (size is None) and (footprint is None):
+        return array
+    if footprint is None:
+        footprint = np.ones((size, size))
+    sig = signature(filt)
+    for kwarg in ('structure', 'footprint'):
+        if kwarg in sig.parameters.keys():
+            break
+    return filt(array, **{kwarg: footprint})
+
+
 def skymask(
     arrays,
-    dilate_mean=None,
-    opening_radius=None,
     percentile=90,
-    extent_cutoff=0.03,
-    coverage_cutoff=0.9,
-    vertical_coverage_cutoff=0.9,
+    edge_params=MappingProxyType({'sigma': 3}),
+    median=MappingProxyType({'input': 5, 'segments': None}),
+    cutoffs=MappingProxyType(
+        {'extent': 0.05, 'coverage': None, 'v': 0.9, 'h': None}
+    ),
+    input_mask_dilation=None,
     floodfill=True,
-    clear_above=True,
-    cut_below=True,
+    refit_params=MappingProxyType(
+        {'clear': True, 'cut': True, 'cut_depth': 5, 'edge_sigma': None}
+    ),
 ):
-    mean = _get_flat_mean(arrays, dilate_mean)
-    segments = centile_threshold(mean, percentile)
-    if opening_radius is not None:
-        segments = ndi.binary_opening(
-            segments, dilation_kernel(opening_radius * 2, sharp=True)
-        )
+    mean = maybe_filter(
+        _get_flat_mean(arrays, input_mask_dilation), median.get('input')
+    )
+    edges = outline(mean, **edge_params)
+    if percentile is not None:
+        segments = centile_threshold(mean, percentile)
+    else:
+        segments = np.ones(mean.shape)
+    segments[np.nonzero(edges)] = 0
+    segments = maybe_filter(segments, median.get('segments'))
     mask = pluck_label(segments)
-    ok, filled, extent = True, None, None
-    if extent_cutoff is not None:
-        ok, extent, result = test_extent(mask, extent_cutoff, extent)
-        if ok is False:
-            return _blank(mask.shape)
-    if coverage_cutoff is not None:
-        ok, extent, filled, result = test_coverage(
-            mask, coverage_cutoff, extent
-        )
-        if ok is False:
-            return _blank(mask.shape)
-    if not (
-        (vertical_coverage_cutoff is not None) or clear_above or cut_below
-    ):
-        if floodfill is True:
-            if filled is None:
-                return floodfill_mask(mask)
-            else:
-                return filled
-        return mask
-    widest_row_ix = find_widest_row(mask)
-    widest_row = mask[widest_row_ix]
-    w_col_indices = get_widest_section(widest_row)
-    if vertical_coverage_cutoff is not None:
-        ok, result = test_vertical_coverage(
-            mask, widest_row_ix, w_col_indices, vertical_coverage_cutoff
-        )
-        if ok is False:
-            return _blank(mask.shape)
-    # show(mask)
-    if clear_above is True:
-        mask[:widest_row_ix, w_col_indices[0] : w_col_indices[1]] = 1
-    # show(mask)
-    if cut_below is True:
-        below = ~mask[widest_row_ix:]
-        row_x = np.arange(w_col_indices[0] + 1, w_col_indices[1])
-        bounds = [np.nonzero(below[:, x])[0][0] for x in row_x]
-        for bound, x in zip(bounds, row_x):
-            mask[(widest_row_ix + bound):, x] = 0
-        mask = pluck_label(ndi.binary_erosion(mask, dilation_kernel(6)))
+    if check_mask_validity(mask, **cutoffs) is False:
+        return _blank(mask.shape)
+    mask[edges] = 1
+    mask = refit_mask(mask, mean, edges, **refit_params)
     if floodfill is True:
         return floodfill_mask(mask)
     return mask
 
 
-def _get_flat_mean(arrays, dilate_mean):
+def refit_mask(
+    mask,
+    base,
+    edges=None,
+    clear=True,
+    cut=True,
+    cut_depth=5,
+    edge_sigma=None
+):
+    if (clear is False) and (cut is False):
+        return mask
+    w_col_ix, w_row_ix = widest_part(mask)
+    if clear is True:
+        mask[:w_row_ix, w_col_ix[0]: w_col_ix[1]] = 1
+    if cut is True:
+        if edges is None:
+            edges = outline(base[w_row_ix:])
+        else:
+            edges = edges[w_row_ix:]
+        if edge_sigma is not None:
+            edges = ndi.gaussian_filter(edges.astype('uint8'), sigma=edge_sigma)
+        below = np.cumsum(edges, axis=0)
+        # row_x = np.arange(w_col_ix[0] + 1, w_col_ix[1])
+        row_x = np.arange(0, mask.shape[1])
+        bounds = {}
+        for x in row_x:
+            bound = np.nonzero(below[:, x] >= cut_depth)[0]
+            if len(bound) > 0:
+                bounds[x] = bound[0]
+        # bounds = [np.nonzero(below[:, x] >= cut_depth)[0][0] for x in row_x]
+        for x, bound in bounds.items():
+            mask[(w_row_ix + bound):, x] = 0
+    return pluck_label(mask)
+
+
+def widest_part(mask):
+    widest_row_ix = find_widest_row(mask)
+    widest_row = mask[widest_row_ix]
+    w_col_indices = get_widest_section(widest_row)
+    return w_col_indices, widest_row_ix
+
+
+def check_mask_validity(mask, extent=None, coverage=None, v=None, h=None):
+    for check, threshold in zip(
+        (test_extent, test_coverage, test_axes),
+        (extent, coverage, (v, h))
+    ):
+        if check(mask, threshold) is False:
+            return False
+    return True
+
+
+def _get_flat_mean(arrays, dilate_mean=None):
     if all([isinstance(a, np.ma.MaskedArray) for a in arrays]):
         mean = _flat_masked_mean(arrays, dilate_mean)
     else:
@@ -189,7 +248,7 @@ def _get_flat_mean(arrays, dilate_mean):
     return mean
 
 
-def _flat_masked_mean(arrays, dilate):
+def _flat_masked_mean(arrays, dilate=None):
     if dilate is not None:
         mask = flatmask(arrays, dilate=dilate)
     else:
