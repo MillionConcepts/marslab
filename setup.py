d6294ae2930025721388ff53c3d9d224e45e76c1
Sierra Brown <sierra@millionconcepts.com>
2023-12-06 15:38:28 -0500

    bump version to 0.9.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="marslab",
-    version="0.9.12",
+    version="0.9.13",
     url="https://github.com/millionconcepts/marslab.git",
     author="Million Concepts",
     author_email="chase@millionconcepts.com",
